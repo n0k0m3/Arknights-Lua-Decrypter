3229a526b074b38c2c9422645a79772f21faabcc
Minh Nguyen <10925298+n0k0m3@users.noreply.github.com>
2020-02-05 08:47:13 -0500

    Playing around

--- jsontest.py
+++ jsontest.py
@@ -1,8 +1,15 @@
 import orjson
 
-with open('skill_table.json','rb') as file:
-    data=file.read()
-    data=orjson.loads(data)
-    print(data)
-    for skill in data:
-        print(data[skill])
+# with open('skill_table.json','rb') as file:
+#     data=file.read()
+#     data=orjson.loads(data)
+#     print(data)
+#     for skill in data:
+#         print(data[skill])
+        
+b='n9u0CnXyf8iSNnSnUXjnO8'
+b=b[::-1]
+b=b.replace('f','').replace('n','')
+print(b)
+if b == '8OjXUSNSi8yXC0u9':
+    print('yes')
